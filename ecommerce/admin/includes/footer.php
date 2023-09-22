fd5adfef0e20aafe6500d6d5894b26023c9f8734
Md Akbar Hossain <53491547+akborhossain@users.noreply.github.com>
2023-09-22 14:02:34 +0600

    Delete ecommerce directory

--- ecommerce/admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <strong>Copyright &copy; 2021 Brought To You By<a href="#"> Taj Bangla</a></strong>
-</footer>
